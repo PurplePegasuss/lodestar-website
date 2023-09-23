1dc0430c4384e006f2bb554cec0ea3e2076fdece
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-09-23 17:42:20 +0300

    remove feedback section

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,45 +1,38 @@
-import { Box, Card, CardBody, HStack, Heading, Image, Text, VStack } from '@chakra-ui/react';
+import { Box, HStack, Heading, Image, Text, VStack } from '@chakra-ui/react';
 import NamedSection from '../components/layout/named_section';
 
 const Contact = () => (
-  <>
-    <NamedSection name="Контакты">
-      <HStack justify="center" gap="10">
-        <VStack alignItems="stretch" gap="5">
-          <Box>
-            <Heading as="h3" size="lg">
-              Адрес
-            </Heading>
-            <Text>г. Иннополис, ...</Text>
-          </Box>
-          <Box>
-            <Heading as="h3" size="lg">
-              Email
-            </Heading>
-            <Text>lodestar@please-replace-this-domain.com</Text>
-          </Box>
-          <Box>
-            <Heading as="h3" size="lg">
-              Телефон
-            </Heading>
-            <Text>+79876543210</Text>
-          </Box>
-          <Box>
-            <Heading as="h3" size="lg">
-              Социальные сети
-            </Heading>
-            <Text>....</Text>
-          </Box>
-        </VStack>
-        <Image src="/img/map.png" />
-      </HStack>
-    </NamedSection>
-    <NamedSection name="Обратная связь">
-      <Card>
-        <CardBody>Тут должна быть форма</CardBody>
-      </Card>
-    </NamedSection>
-  </>
+  <NamedSection name="Контакты">
+    <HStack justify="center" gap="10">
+      <VStack alignItems="stretch" gap="5">
+        <Box>
+          <Heading as="h3" size="lg">
+            Адрес
+          </Heading>
+          <Text>г. Иннополис, ...</Text>
+        </Box>
+        <Box>
+          <Heading as="h3" size="lg">
+            Email
+          </Heading>
+          <Text>lodestar@please-replace-this-domain.com</Text>
+        </Box>
+        <Box>
+          <Heading as="h3" size="lg">
+            Телефон
+          </Heading>
+          <Text>+79876543210</Text>
+        </Box>
+        <Box>
+          <Heading as="h3" size="lg">
+            Социальные сети
+          </Heading>
+          <Text>....</Text>
+        </Box>
+      </VStack>
+      <Image src="/img/map.png" />
+    </HStack>
+  </NamedSection>
 );
 
 export default Contact;
