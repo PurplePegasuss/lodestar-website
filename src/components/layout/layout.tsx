c677c9d46282f406291fba4442d056d473590df3
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-09-28 16:51:28 +0300

    fix height for short pages

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { Spacer, VStack } from '@chakra-ui/react';
 import Header from './header';
 import Footer from './footer';
 
 const Layout = () => (
-  <>
+  <VStack spacing="0" minHeight="100vh" alignItems="stretch">
     <Header />
     <Outlet />
+    <Spacer />
     <Footer />
-  </>
+  </VStack>
 );
 
 export default Layout;
