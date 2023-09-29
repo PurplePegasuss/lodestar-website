9a519bf6a7a305d15d311e2f36359f328e90923d
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-09-29 08:53:55 +0300

    fix /projects layout

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,15 +1,15 @@
-import { VStack } from '@chakra-ui/react';
+import { HStack } from '@chakra-ui/react';
 import ProjectCard from '../../components/project_card';
 import NamedSection from '../../components/layout/named_section';
 import projects from '../../data/projects';
 
 const Projects = () => (
   <NamedSection name="Проекты">
-    <VStack spacing="20">
+    <HStack spacing="10" flexWrap="wrap" alignItems="stretch">
       {projects.map((project) => (
         <ProjectCard key={project.slug} project={project} />
       ))}
-    </VStack>
+    </HStack>
   </NamedSection>
 );
 
